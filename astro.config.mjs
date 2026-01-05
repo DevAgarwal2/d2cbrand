4ddd201ec69a662dbf2cdef7dda8ef9838b5ad0c
Dev <devagarwal076@gmail.com>
2026-01-05 15:56:07 +0530

    server side added

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-
 import sitemap from '@astrojs/sitemap';
-
-
 import netlify from '@astrojs/netlify';
-
 
 // https://astro.build/config
 export default defineConfig({
